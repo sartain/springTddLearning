8964c567630bad7db5856d038a249c020a3a7288
Sartain, Alex <alex.sartain@zuhlke.com>
2022-03-01 13:58:00 +0000

    Add spring initialized config

---
+++ src/test/java/com/alex/springtdd/SpringtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
